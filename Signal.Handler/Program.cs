195b2396837dbb7649987e0729abe398ae92bf7d
Adelin Chis <adelingabriel08@gmail.com>
2022-04-10 23:24:20 +0300

    fix host

--- Signal.Handler/Program.cs
+++ Signal.Handler/Program.cs
@@ -3,37 +3,30 @@ using Microsoft.Extensions.Hosting;
 using System.IO.Ports;
 using Signal.Handler;
 
-using IHost host = Host.CreateDefaultBuilder(args).Build();
-
 IConfiguration config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json")
     .Build();
 
-await host.RunAsync();
-
 var serialPortOptions = new SerialPortOptions();
 config.Bind(nameof(SerialPortOptions), serialPortOptions);
-
 var serialPort = new SerialPort(serialPortOptions.PortName, serialPortOptions.BaudRate);
-
 serialPort.Open();
 
 if (serialPort.IsOpen)
 {
-    Console.WriteLine("Listening on port {0}", config["SerialPort:PortName"]);
+    Console.WriteLine("Listening on port {0}", serialPortOptions.PortName);
 }
 else
 {
     throw new Exception($"Cannot connect to port {config["SerialPort:PortName"]}");
 }
-Console.WriteLine("im here");
 while (true)
 {
     try
     {
         var status = serialPort.ReadLine();
-        switch (status)
+        switch (status.Trim())
         {
             case "0":
                 Console.WriteLine("Door closed!");
